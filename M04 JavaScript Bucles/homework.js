02d20f6ffa752839ba1fffe482e9ba491b15b6ca
miltonzini <miltonzini@gmail.com>
2023-02-11 19:34:15 -0300

    complete more JS exercises

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -120,6 +120,11 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 1 === 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function fizzBuzz(num) {
@@ -128,6 +133,18 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+   // TO DO: continue
+   if (num % 3 === 0) {
+      return "fizz";
+   } else if (num % 5 === 0) {
+      return "buzz";
+   } else if (num % 3 === 0 && num % 5 === 0) {
+      return "fizzbuzz";
+   } else {
+      return false;
+   }
+
+
 }
 
 function operadoresLogicos(num1, num2, num3) {
