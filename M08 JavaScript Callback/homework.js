332e0047bee304d25c7cf8460adcc06e03c4dfe1
miltonzini <miltonzini@gmail.com>
2023-03-11 20:00:31 -0300

    complete JS exercises

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -50,6 +50,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach(elemento => {
+      cb(elemento);
+   });
 }
 
 function map(array, cb) {
@@ -57,12 +60,26 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+
+   var nuevoArray = array.map((elemento) => {
+      return cb(elemento);
+   });
+   return nuevoArray;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var nuevoArray = [];
+ 
+    for (let i = 0; i < arrayOfStrings.length; i++) {
+       if (arrayOfStrings[i][0] === "a") {
+          nuevoArray.push(arrayOfStrings[i]);
+       }
+    }
+
+    return nuevoArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
