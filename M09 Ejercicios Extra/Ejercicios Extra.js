30950b2390669cedca339ec49c61feb48336b3b4
miltonzini <miltonzini@gmail.com>
2023-03-19 18:19:05 -0300

    complete exercise

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -23,6 +23,21 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var array = string.split('');
+   var objeto = {};
+   var caracteresSinRepetir = [];
+   for (let i = 0; i < array.length; i++) {
+      if (!caracteresSinRepetir.includes(array[i])) caracteresSinRepetir.push(array[i]);
+   }
+   var caracteresOrdenados = caracteresSinRepetir.sort();
+   for (let i = 0; i < caracteresOrdenados.length; i++) {
+      var suma = 0;
+      for (let j = 0; j < array.length; j++) {
+         if (array[j] === caracteresOrdenados[i]) suma++;
+      };
+      objeto[caracteresOrdenados[i]] = suma;
+   };
+   return objeto;
 }
 
 function capToFront(string) {
