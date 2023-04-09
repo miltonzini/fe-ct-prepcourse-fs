f45ee4baead111bf1f60d432bf611845ecbf16d8
miltonzini <miltonzini@gmail.com>
2023-04-09 18:11:35 -0300

    complete JS exercise

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -116,6 +116,23 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   var nuevoArray = [];
+   var cantidadDeLetras = [];
+   
+   for (let i = 0; i < arrayOfStrings.length; i++) {
+      var length = arrayOfStrings[i].length;
+      if (!cantidadDeLetras.includes(length)) cantidadDeLetras.push(length);
+   }
+   
+   var cantidadDeLetrasOrdenado = cantidadDeLetras.sort((a, b) => a - b);
+
+   for (let i = 0; i < cantidadDeLetrasOrdenado.length; i++) {
+      for (let j = 0; j < arrayOfStrings.length; j++) {
+            if (arrayOfStrings[j].length == cantidadDeLetrasOrdenado[i]) nuevoArray.push(arrayOfStrings[j]);
+      }
+   }
+   
+   return nuevoArray;
 }
 
 function buscoInterseccion(array1, array2) {
